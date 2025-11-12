149665d3d191584008020e7a9b3d56e359f079cd
Zoryas <zoryas@gmail.com>
2025-11-12 11:57:06 +0800

    style update

--- src/App.js
+++ src/App.js
@@ -2,31 +2,37 @@ import React, { useState } from "react";
 import TaskForm from "./components/TaskForm";
 import TaskList from "./components/TaskList";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 
 function App() {
   const [refresh, setRefresh] = useState(false);
   const [editingTask, setEditingTask] = useState(null);
 
   const handleTaskAdded = () => setRefresh(!refresh);
-
-  const handleEditTask = (task) => {
-    setEditingTask(task);
-  };
-
-  const handleCancelEdit = () => {
-    setEditingTask(null);
-  };
+  const handleEditTask = (task) => setEditingTask(task);
+  const handleCancelEdit = () => setEditingTask(null);
 
   return (
-    <Container className='py-4'>
+    <Container fluid className='py-4'>
       <h1 className='text-center mb-4 fade-in app-title'>Task Lab Exam</h1>
-      <TaskForm
-        onTaskAdded={handleTaskAdded}
-        editTask={editingTask}
-        onCancelEdit={handleCancelEdit}
-      />
-      <TaskList refresh={refresh} onEditTask={handleEditTask} />
+
+      <Row>
+        <Col md={4} lg={3}>
+          <div className='sticky-form'>
+            <TaskForm
+              onTaskAdded={handleTaskAdded}
+              editTask={editingTask}
+              onCancelEdit={handleCancelEdit}
+            />
+          </div>
+        </Col>
+        <Col md={8} lg={9}>
+          <TaskList refresh={refresh} onEditTask={handleEditTask} />
+        </Col>
+      </Row>
     </Container>
   );
 }
